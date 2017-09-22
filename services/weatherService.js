5df419dea565313d102ed98f8fbdb588a258090f
cmatzenbach <cmatzenbach@gmail.com>
2017-09-22 03:33:23 -0400

    Refactored service methods out of index and created services folder and weather services file

--- services/weatherService.js
+++ services/weatherService.js
@@ -1,15 +1,15 @@
-const express = require('express');
 const mongoose = require('mongoose');
 const http = require('http');
-const keys = require('./config/keys');
-const app = express();
-require('./models/Weather');
-require('./services/weatherService');
+const keys = require('../config/keys');
 
+const Weather = mongoose.models('weatherdata'); // is this the correct reference?
+
+// set up mongodb connection
 // mongoose.connect('mongodb://mongo:27017/weather');
 mongoose.connect(keys.mongoUri);
 const db = mongoose.connection;
 
+// set up wunderground api connection values
 var options = {
   host: 'api.wunderground.com',
   path: '/api/' + keys.wundergroundKey + '/conditions/q/33143.json',
@@ -17,8 +17,12 @@ var options = {
   port: 80
 };
 
-app.get('/', function(req, res) {
-  // res.send({ hi: 'there' });
+// function to call out to wunderground api and store result in mongo
+// is this refactored correctly? (min 5:00 of "Saving Model Instances")
+// need to create a new instance of weather
+// need to check to see if data is already in mongo; if it is and is less than an hour old,
+// then don't overwrite record in mongo (min 1:30 of Mongoose Queries)
+function storeAreaWeather(options, db) {
 
   var callback = function(response) {
     var str = '';
@@ -28,17 +32,15 @@ app.get('/', function(req, res) {
     response.on('end', function() {
       var jsonResult = JSON.parse(str);
 
+      // new Weather({ temp: jsonsource.temp, city: jsonsource: city, ... }).save();
       db.collection('weatherdata').save(jsonResult, function(err, records) {
         if (err) throw err;
         console.log('record added');
       });
 
-      res.send(jsonResult);
     });
 
   };
 
   http.request(options, callback).end();
-});
-
-app.listen(5000);
+}
