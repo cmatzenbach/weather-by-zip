a9b685e433ac722f25ba4307347ebafdbba67e34
cmatzenbach <cmatzenbach@gmail.com>
2017-09-27 16:54:32 -0400

    Added bash install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# run npm install for backend dependencies
+npm install
+
+# switch to client folder and install react dependencies
+cd /client
+npm install
+
+# switch back to root folder and run application
+cd ../
+npm run dev
